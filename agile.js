07200bf1f75d43cb89ca0e5b250c2f6766b4047a
Maxim <maxim.akristiniy@gmail.com>
2019-11-21 19:57:42 +0300

    Update agile.js

--- agile.js
+++ agile.js
@@ -1,36 +1,36 @@
 function agile(tickets: Tickets, developers:Developer[], manager:Manager){
-	while(tickets.count>0){
-		for (developer in developers){
-			if (developer.dead){
-				developers.remove(developer);
-				const newHorse: Developer = HR.hire('devloper');
-				developers.add(newHorse);
-				developer = newHorse;
-			}
-		}
+    while(tickets.count>0){
+        for (developer in developers){
+            if (developer.dead){
+                developers.remove(developer);
+                const newHorse: Developer = HR.hire('devloper');
+                developers.add(newHorse);
+                developer = newHorse;
+            }
+        }
 
-		for (developer in developers){
-			if(developer.busy){
-				if(developer.StoryPoints > (developer.StoryPointsEstimate*5)){
-					manager.kick(developer);
-					manager.drink();
-					developer.busy = false;
-					developer.last_ticket = null;
-				}
-			}
-			if(!developer.busy){
-				if(developer.last_ticket!=null){
-					tickets.remove(developer.last_ticket);
-				}
-			}
-		}
+        for (developer in developers){
+            if(developer.busy){
+                if(developer.StoryPoints > (developer.StoryPointsEstimate*5)){
+                    manager.kick(developer);
+                    manager.drink();
+                    developer.busy = false;
+                    developer.last_ticket = null;
+                }
+            }
+            if(!developer.busy){
+                if(developer.last_ticket!=null){
+                    tickets.remove(developer.last_ticket);
+                }
+            }
+        }
 
-		for (ticket in tickets){
-			for (developer in developers){
-				if(!developer.busy){
-					developer.startResolveNextTicket(ticket);
-				}
-			}
-		}
-	}
+        for (ticket in tickets){
+            for (developer in developers){
+                if(!developer.busy){
+                    developer.startResolveNextTicket(ticket);
+                }
+            }
+        }
+    }
 }
